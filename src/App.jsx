7c5b32543ab26ccf32175830ab542425819a0331
s0urce-c0de <email.kanavg@gmail.com>
2023-06-08 07:59:11 -0700

    updating css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Title from './Title/Title.jsx'
-import Card from './Card/Card.jsx'
+// import Card from './Card/Card.jsx'
 import Create_Cards from './Create_Cards/Create_Cards.jsx'
 import Marquee from './Marquee/Marquee.jsx'
 import React from 'react'
@@ -8,14 +8,14 @@ import './App.css'
 const my_games = [
   {
     title: 'Spaceship Game',
-    image: '/favicon.svg',
-    url: "#",
+    image: './favicon.svg',
+    url: "https://replit.com/@KanavGupta7/SpaceShip-Game?v=1",
     img_alt: "Image not found"
   },
   {
     title: 'SVG Clock',
-    image: '/favicon.svg',
-    url: "#",
+    image: './favicon.svg',
+    url: "https://s0urce-c0de.github.io/SVG-Clock",
     img_alt: "Image not found"
   }
 ]
@@ -29,8 +29,6 @@ export default function App(props) {
       <Title>Welcome to my games website!</Title>      
       <Marquee>
         {cards}
-        <Card title="Spaceship Game" img="/favicon.svg" href='https://replit.com/@KanavGupta7/SpaceShip-Game?v=1' img-alt=""/>
-        <Card title="Spaceship Game" img="/favicon.svg" href='https://replit.com/@KanavGupta7/SpaceShip-Game?v=1' img-alt=""/>
       </Marquee>
     </div>
   )
