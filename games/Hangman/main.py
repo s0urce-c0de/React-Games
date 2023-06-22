4c835f8ec51c35996ff79d1830628b6ef45540e5
s0urce-c0de <email.kanavg@gmail.com>
2023-06-22 08:15:15 -0700

    working...

--- games/Hangman/main.py
+++ games/Hangman/main.py
@@ -4,21 +4,24 @@
 import pygame
 import types
 import multiprocessing
+multiprocessing.set_start_method('fork')
+import functools 
+import random
 from pathlib import Path
 
+if __name__=="__main__":
+  manager=multiprocessing.Manager()
+
 pygame.init()
+
 clock=pygame.time.Clock()
 pardir=Path(__file__).resolve().parent
-manager = multiprocessing.Manager()
-shared_dict = manager.dict()
 game_window = pygame.display.set_mode((900, 500),
                                       pygame.RESIZABLE)
-WINDOW_WIDTH=game_window.get_width()
-WINDOW_HEIGHT=game_window.get_height()
 
-images=[]
+images=manager.list([])
 for i in range(0, 7):
-   images[i]=(pygame.transform.scale(pygame.image.load(pardir/'images'/f'Hangman{i}.png'), (WINDOW_WIDTH//(900/209), WINDOW_HEIGHT/(500/216))))
+   images.append(pygame.image.tostring(pygame.transform.scale(pygame.image.load(pardir/'images'/f'Hangman{i}.png'), (game_window.get_width()//(900/209), game_window.get_height()/(500/216))), 'RGB'))
 
 words_file=open(pardir.parent / 'words.txt')
 words=words_file.read().split('\n')
@@ -46,7 +49,7 @@ class Image(pygame.sprite.Sprite):
 class Button:
     def __init__(self, x: int, y: int, radius: int, thickness: int =1, color = "#000000", \
                  image: pygame.surface.Surface = pygame.font.SysFont(None, 0).render('', True, "#000000"), \
-                 onClick: types.FunctionType = (lambda: 1), *onClickArgs, **onClickKwargs):
+                 onClick: types.FunctionType = (lambda: None), *onClickArgs, **onClickKwargs):
         self.x = x
         self.y = y
         self.radius = radius
@@ -61,7 +64,7 @@ class Button:
         self.image = pygame.transform.scale(self.image_raw, (2*self.radius, 2*self.radius))
         self.clicked=False
         self.image.blit(self.image, (0,0), (0,0,self.radius*2,self.radius*2))
-
+        self.should_draw=True
     def draw(self, screen):
         mouse_pos = pygame.mouse.get_pos()
         mouse_clicked = pygame.mouse.get_pressed()[0]
@@ -70,65 +73,82 @@ class Button:
         screen.blit(self.image, (self.x-self.radius, self.y-self.radius))
         
         if hover and mouse_clicked and not self.clicked:
-            self.onclick(*self.onclickargs, **self.onclickkwargs)
-            self.clicked=True
+          self.onclick(*self.onclickargs, **self.onclickkwargs)
+          self.clicked=True
         elif not mouse_clicked:
-           self.clicked=False
+          self.clicked=False
     
     def is_hover(self, mouse_pos):
         distance = ((self.x - mouse_pos[0])**2 + (self.y - mouse_pos[1])**2)**0.5
         return distance <= self.radius
+def run_on_subprocess(func):
+    @functools.wraps(func)
+    def _wrapper(*args, **kwargs):
+        sub_p = multiprocessing.Process(target=func, args=args, kwargs=kwargs)
 
-button_count=0
-buttons=[]
-for char in range(ord('A'), ord('Z')+1):
-   button_count+=1
-   win_width=WINDOW_WIDTH//15*15
+        return sub_p
+    return _wrapper
+def update_char(char: str):
+  
+  if char in ''.join(word).upper():
+    for i in len(word):
+      if ''.join(word).upper()[i]==char:
+        word[i] = char
+  print('Word:',guessed_word)
+buttons={}
+for char in range(65, 91):
+   win_width=game_window.get_width()//15*15
    unit=win_width/15
-   radius=((WINDOW_HEIGHT-BY.get_height()-TITLE.get_height()-images[incorrect].get_height()-30))//8 - 1
-   y_buffer = radius*((button_count-1)//13)+60
-   x=unit+win_width-(win_width-((button_count-1)%13)*unit)
-   y=BY.get_height()+TITLE.get_height()+images[incorrect].get_height()+30*((button_count-1)//13)+y_buffer
-   buttons.append(Button(x, y, radius, 3, '#000000', XL_FONT.render(chr(char), True, '#000000')))
-del button_count, win_width, unit, radius, y_buffer, x, y
-
-if __name__ == "__main__":
-  run = True
-  while run:
-
-    images=[]
-    for i in range(0, 7):
-      images.append(pygame.transform.scale(pygame.image.load(pardir/'images'/f'Hangman{i}.png'), (WINDOW_WIDTH//(900/209), WINDOW_HEIGHT/(500/216))))
-    # reset size 4 resizable  window
-    WINDOW_WIDTH=game_window.get_width()
-    WINDOW_HEIGHT=game_window.get_height()
-    # reset level text
-    LEVEL_TEXT=LEVEL_FONT.render(f"Level: {level}", True, '#000000')
-    # set background
-    game_window.fill("#FFFFFF")
-    # title
-    game_window.blit(TITLE, ((WINDOW_WIDTH-TITLE.get_width())/2, 3))
-    # by
-    game_window.blit(BY, ((WINDOW_WIDTH-TITLE.get_width())/2+5, 7+TITLE.get_height()))
-    # level
-    game_window.blit(LEVEL_TEXT, (WINDOW_WIDTH-LEVEL_TEXT.get_width(), 2))
-    # hanged man
-    game_window.blit(images[incorrect], ((WINDOW_WIDTH-images[incorrect].get_width())/2-WINDOW_WIDTH/6//1, TITLE.get_height()+50))
-    for i in range(0, 26):
-      button=buttons[i]
-      win_width=WINDOW_WIDTH//15*15
-      unit=win_width/15
-      button.radius=((WINDOW_HEIGHT-BY.get_height()-TITLE.get_height()-images[incorrect].get_height()-30))//8 - 1
-      y_buffer = button.radius*(i//13)+60
-      button.x=unit+win_width-(win_width-(i%13)*unit)
-      button.y=BY.get_height()+TITLE.get_height()+images[incorrect].get_height()+30*(i//13)+y_buffer
-      button.image = pygame.transform.scale(button.image_raw, (2*button.radius, 2*button.radius))
-      buttons[i]=button
-
-    for buttone in buttons:
-       buttone.draw(game_window)
-    pygame.display.update()
-    for event in pygame.event.get():
-      if event.type == 256:
-          pygame.quit()
-          exit()
+   radius=((game_window.get_height()-BY.get_height()-TITLE.get_height()-(game_window.get_height()/(500/216))-30))//8 - 1
+   x=unit+win_width-(win_width-((char-65)%13)*unit)
+   y=BY.get_height()+TITLE.get_height()+(game_window.get_height()/(500/216))+2*radius*((char-65)//13) + 90
+   buttons[chr(char)]=Button(x, y, radius, 3, '#000000', XL_FONT.render(chr(char), True, '#000000'), update_char, chr(char))
+# def update_images():
+#   while run.value:
+#     for i in range(0, 7):
+#       images[i]=pygame.image.tostring(pygame.transform.scale(pygame.image.load(pardir/'images'/f'Hangman{i}.png'), (game_window.get_width()//(900/209), game_window.get_height()/(500/216))))
+del win_width, unit, radius, x, y
+try:
+  if __name__ == "__main__":
+    word = manager.list(random.choice(words))
+    guessed_word=manager.list('_'*len(word))
+    run = manager.Value('i', True)
+    #p1=multiprocessing.Process(target=update_images)
+    print(word)
+    #p1.start()
+    while run.value:
+      # reset level text
+      LEVEL_TEXT=LEVEL_FONT.render(f"Level: {level}", True, '#000000')
+      # set background
+      game_window.fill("#FFFFFF")
+      # title
+      game_window.blit(TITLE, ((game_window.get_width()-TITLE.get_width())/2, 3))
+      # by
+      game_window.blit(BY, ((game_window.get_width()-TITLE.get_width())/2+5, 7+TITLE.get_height()))
+      # level
+      game_window.blit(LEVEL_TEXT, (game_window.get_width()-LEVEL_TEXT.get_width(), 2))
+      # hanged man
+      print(pygame.transform.scale((pygame.image.fromstring(images[incorrect], (3344, 3456), 'RGB'), (((game_window.get_width()-game_window.get_height()/(500/216)))/2-game_window.get_width()/6)//1, TITLE.get_height()+50), (int(game_window.get_width()//(900/209)), int(game_window.get_height()//(500/216)))))
+      game_window.blit(pygame.transform.scale((pygame.image.fromstring(images[incorrect], (3456, 3344), 'RGB'), (((game_window.get_width()-game_window.get_height()/(500/216)))/2-game_window.get_width()/6)//1, TITLE.get_height()+50), (int(game_window.get_width()//(900/209)), int(game_window.get_height()//(500/216)))))
+      # for i in range(65, 91):
+      #   button=buttons[chr(i)]
+      #   win_width=game_window.get_width()//15*15
+      #   unit=win_width/15
+      #   button.radius=((game_window.get_height()-BY.get_height()-TITLE.get_height()-images[incorrect].get_height()-30))//8 - 1
+      #   button.x=unit+win_width-(win_width-((i-65)%13)*unit)
+      #   button.y=BY.get_height()+TITLE.get_height()+images[incorrect].get_height()+2*button.radius*((i-65)//13)+90
+      #   button.image = pygame.transform.scale(button.image_raw, (2*button.radius, 2*button.radius))
+      #   button.onclickargs=(button.onclickargs[0])
+      #   buttons[chr(i)]=button
+      for key in buttons:
+        if buttons[key].should_draw: buttons[key].draw(game_window)
+      pygame.display.update()
+      for event in pygame.event.get():
+        if event.type == 256:
+          run.value=False
+    pygame.quit()
+    exit()
+finally:
+  pygame.quit()
+  raise
+  exit()
--- games/Hangman/main2.py
+++ games/Hangman/main2.py
@@ -1,134 +1,43 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
-
+import dill
 import pygame
-import types
-import multiprocessing
-from pathlib import Path
+import io
 
-pygame.init()
-clock=pygame.time.Clock()
-pardir=Path(__file__).resolve().parent
-manager = multiprocessing.Manager()
-shared_dict = manager.dict()
-shared_dict["game_window"] = pygame.display.set_mode((900, 500),
-                                      pygame.RESIZABLE)
-shared_dict["WINDOW_WIDTH"]=shared_dict["game_window"].get_width()
-shared_dict["WINDOW_HEIGHT"]=shared_dict["game_window"].get_height()
-
-images=[]
-for i in range(0, 7):
-   images.append(pygame.transform.scale(pygame.image.load(pardir/'images'/f'Hangman{i}.png'), (shared_dict["WINDOW_WIDTH"]//(900/209), shared_dict["WINDOW_HEIGHT"]/(500/216))))
-shared_dict["images"] = images
-
-words_file=open(pardir.parent / 'words.txt')
-words=words_file.read().split('\n')
-words_file.close()
-del words_file
-
-level=1
-incorrect=0
-NORMAL_FONT = pygame.font.SysFont('comicsans', 32)
-MINI_FONT = pygame.font.SysFont('comicsans', 13)
-LEVEL_FONT = pygame.font.SysFont('comicsans', 25)
-XL_FONT = pygame.font.SysFont('comicsans', 3487)
-TITLE=NORMAL_FONT.render("Hangman", True, '#000000')
-BY=MINI_FONT.render("By Kanav G.", True, '#000000')
-LEVEL_TEXT=LEVEL_FONT.render(f"Level: {level}", True, '#000000')
-shared_dict["level"] = level
-shared_dict["incorrect"] = incorrect
-shared_dict["NORMAL_FONT"] = NORMAL_FONT
-shared_dict["MINI_FONT"] = MINI_FONT
-shared_dict["LEVEL_FONT"] = LEVEL_FONT
-shared_dict["XL_FONT"] = XL_FONT
-shared_dict["TITLE"] = TITLE
-shared_dict["BY"] = BY
-shared_dict["LEVEL_TEXT"] = LEVEL_TEXT
-
-class Image(pygame.sprite.Sprite):
-  def __init__(self, x, y, image):
-    super().__init__()
-    self.x = x
-    self.y = y
-    self.image = image
-    self.rect = self.image.get_rect()
-
-class Button:
-    def __init__(self, x: int, y: int, radius: int, thickness: int =1, color = "#000000", \
-                 image: pygame.surface.Surface = pygame.font.SysFont(None, 0).render('', True, "#000000"), \
-                 onClick: types.FunctionType = (lambda: 1), *onClickArgs, **onClickKwargs):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.thickness = thickness
-        self.color = color
-        self.image_raw = image
-        self.onclick=onClick
-        self.onclickargs=onClickArgs
-        self.onclickkwargs=onClickKwargs
-        self.image_rect = self.image_raw.get_rect()
-        self.image_rect.center = (self.x, self.y)
-        self.image = pygame.transform.scale(self.image_raw, (2*self.radius, 2*self.radius))
-        self.clicked=False
-        self.image.blit(self.image, (0,0), (0,0,self.radius*2,self.radius*2))
-
-    def draw(self, screen):
-        mouse_pos = pygame.mouse.get_pos()
-        mouse_clicked = pygame.mouse.get_pressed()[0]
-        hover = self.is_hover(mouse_pos)
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, self.thickness)
-        screen.blit(self.image, (self.x-self.radius, self.y-self.radius))
-        
-        if hover and mouse_clicked and not self.clicked:
-            self.onclick(*self.onclickargs, **self.onclickkwargs)
-            self.clicked=True
-        elif not mouse_clicked:
-           self.clicked=False
+def pickleable(obj):
+    # Serialize the object using dill
+    pickled_obj = dill.dumps(obj)
     
-    def is_hover(self, mouse_pos):
-        distance = ((self.x - mouse_pos[0])**2 + (self.y - mouse_pos[1])**2)**0.5
-        return distance <= self.radius
+    # Deserialize the object using dill
+    unpickled_obj = dill.loads(pickled_obj)
+    
+    return unpickled_obj
 
-button_count=0
-buttons=[]
-for char in range(ord('A'), ord('Z')+1):
-   button_count+=1
-   win_width=shared_dict["WINDOW_WIDTH"]//15*15
-   unit=win_width/15
-   radius=((shared_dict["WINDOW_HEIGHT"]-BY.get_height()-TITLE.get_height()-shared_dict["images"][incorrect].get_height()-30))//8 - 1
-   y_buffer = radius*((button_count-1)//13)+60
-   x=unit+win_width-(win_width-((button_count-1)%13)*unit)
-   y=BY.get_height()+TITLE.get_height()+shared_dict["images"][incorrect].get_height()+30*((button_count-1)//13)+y_buffer
-   buttons.append(Button(x, y, radius, 3, '#000000', XL_FONT.render(chr(char), True, '#000000')))
-del button_count, win_width, unit, radius, y_buffer, x, y
+# Pygame example
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption('Pickleable Surface Example')
+    
+    # Create a surface and fill it with a color
+    other_surface = pygame.Surface((100, 100))
+    other_surface.fill((255, 0, 0))  # Red color
+    
+    clock = pygame.time.Clock()
+    running = True
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill((255, 255, 255))  # Fill the screen with a white background
+        
+        # Use the pickleable function in the context of surface.blit()
+        screen.blit(pickleable(other_surface), (320, 240))
+        
+        pygame.display.flip()
+        clock.tick(60)
+    
+    pygame.quit()
 
-if __name__ == "__main__":
-  run = True
-  while run:
-
-    shared_dict["images"]=[]
-    for i in range(0, 7):
-      shared_dict["images"].append(pygame.transform.scale(pygame.image.load(pardir/'images'/f'Hangman{i}.png'), (shared_dict["WINDOW_WIDTH"]//(900/209), shared_dict["WINDOW_HEIGHT"]/(500/216))))
-    # reset size 4 resizable  window
-    shared_dict["WINDOW_WIDTH"]=shared_dict["game_window"].get_width()
-    shared_dict["WINDOW_HEIGHT"]=shared_dict["game_window"].get_height()
-    # reset level text
-    shared_dict["LEVEL_TEXT"]=LEVEL_FONT.render(f"Level: {level}", True, '#000000')
-    # set background
-    shared_dict["game_window"].fill("#FFFFFF")
-    # title
-    shared_dict["game_window"].blit(TITLE, ((shared_dict["WINDOW_WIDTH"]-TITLE.get_width())/2, 3))
-    # by
-    shared_dict["game_window"].blit(BY, ((shared_dict["WINDOW_WIDTH"]-TITLE.get_width())/2+5, 7+TITLE.get_height()))
-    # level
-    shared_dict["game_window"].blit(shared_dict["LEVEL_TEXT"], (shared_dict["WINDOW_WIDTH"]-shared_dict["LEVEL_TEXT"].get_width(), 2))
-    # hanged man
-    shared_dict["game_window"].blit(shared_dict["images"][incorrect], ((shared_dict["WINDOW_WIDTH"]-shared_dict["images"][incorrect].get_width())/2-shared_dict["WINDOW_WIDTH"]/6//1, TITLE.get_height()+50))
-    for i in range(0, 26):
-      button=buttons[i]
-      win_width=shared_dict["WINDOW_WIDTH"]//15*15
-      unit=win_width/15
-      button.radius=((shared_dict["WINDOW_HEIGHT"]-BY.get_height()-TITLE.get_height()-shared_dict["images"][incorrect].get_height()-30))//8 - 1
-      y_buffer = button.radius*(i//13)+60
-      button.x=unit+win_width-(win_width-(i%13)*unit)
-      button.y=BY.get
+if __name__ == '__main__':
+    main()
