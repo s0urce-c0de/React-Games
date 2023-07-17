99a49138defcb867265f48ad4873cff7c23cbac7
s0urce-c0de <email.kanavg@gmail.com>
2023-07-16 19:01:59 -0700

    Hangman game is finished :))

--- games/Hangman/main.py
+++ games/Hangman/main.py
@@ -3,19 +3,14 @@
 
 import pygame
 import importlib
-import functools
 import random
-import types
-import sys
-import os
 from pathlib import Path
 #from functions4pickle import update_char
 #from ..utils import Button, PicklableSurface
-spec = importlib.util.spec_from_file_location("utils", Path(__file__).resolve().parent.parent / 'utils.py')
-utils = importlib.util.module_from_spec(spec)
-sys.modules["utils"] = utils
+spec = importlib.util.spec_from_file_location("utils", Path(__file__).resolve().parent.parent / 'utils.py') # type: ignore
+utils = importlib.util.module_from_spec(spec) # type: ignore
 spec.loader.exec_module(utils)
-Button,PicklableSurface=utils.Button,utils.PicklableSurface
+Button,PicklableSurface,MultiLineText_Blit=utils.CircleButton,utils.PicklableSurface,utils.MultiLineText_Blit
 dev=True
 
 pygame.init()
@@ -24,17 +19,15 @@ pardir=Path(__file__).resolve().parent
 projdir=Path(__file__).resolve().parent.parent.parent
 game_window = pygame.display.set_mode((900, 500),
                                       pygame.RESIZABLE)
-# pygame.display.set_caption("Hangman")
-# pygame.display.set_icon(pygame.image.load(projdir / 'public' / 'favicon.svg'))
+pygame.display.set_caption("Hangman")
+pygame.display.set_icon(pygame.image.load(pardir / 'images' / 'favicon.png'))
 game_window.fill("#FFFFFF")
 pygame.display.update()
 
-# words_file=open(pardir.parent / 'words.txt')
-# words=words_file.read().split('\n')
-# words_file.close()
-# del words_file
-
-words=['FOO', 'BAR']
+words_file=open(pardir.parent / 'words.txt')
+words=words_file.read().split('\n')
+words_file.close()
+del words_file
 
 level=1
 incorrect=0
@@ -46,19 +39,41 @@ BY=MINI_FONT.render("By Kanav G.", True, '#000000')
 LEVEL_TEXT=LEVEL_FONT.render(f"Level: {level}", True, '#000000')
 TITLE=NORMAL_FONT.render("Hangman", True, '#000000')
 
-
-images=[pygame.transform.scale(pygame.image.load(pardir/'images'/f'Hangman{i}.png').convert_alpha(), (game_window.get_width()//(900/209), game_window.get_height()/(500/216))) for i in range(0, 7)]
+raw_images=[pygame.image.load(pardir/'images'/f'Hangman{i}.png') for i in range(0,7)]
+images=[pygame.transform.scale(raw_images[i], (game_window.get_width()//(900/209), game_window.get_height()/(500/216))) for i in range(0, 7)]
 buttons={}
-def update_char(char: str):
+def generate_word(level: int) -> str:
+  generator_level=level
+  if level>15:
+    generator_level=15
+  narrow_word_list=[]
+  for word in words:
+    if generator_level*2<len(word)<generator_level*2+4:
+      narrow_word_list.append(word)
+  return random.choice(narrow_word_list)
+def new_level() -> None:
+  global word, guessed_word, incorrect, level
+  # update level
+  level+=1
+  # reset ammount of incorrect guesses
+  incorrect=0
+  # generate new word
+  word=generate_word(level)
+  print(word)
+  # reset the guessed word
+  guessed_word=['_']*len(word)
+  # regenerate all buttons
+  for button in buttons: buttons[button].should_draw=True
+def update_char(char: str) -> None:
   char=char.upper()
   buttons[char].should_draw=False
-  print(buttons)
-  print(buttons[char])
-  print(char)
   if char in ''.join(word).upper():
     for i in range(len(word)):
-      if word[i]==char:
+      if ''.join(word).upper()[i]==char:
         guessed_word[i]=char
+  else:
+    global incorrect
+    incorrect+=1
 for char in range(65, 91):
    win_width=game_window.get_width()//15*15
    unit=win_width/15
@@ -66,10 +81,13 @@ for char in range(65, 91):
    x=unit+win_width-(win_width-((char-65)%13)*unit)
    y=BY.get_height()+TITLE.get_height()+images[incorrect].get_height()+2*radius*((char-65)//13) + 90
    buttons[chr(char)]=Button(x, y, radius, 3, '#000000', XL_FONT.render(chr(char), True, '#000000').convert_alpha(), update_char, chr(char))
+del win_width, unit, radius, x, y
 def update_images() -> None:
-  images=[pygame.transform.scale(pygame.image.load(pardir/'images'/f'Hangman{i}.png'), (game_window.get_width()//(900/209), game_window.get_height()/(500/216))) for i in range(0,7)]
+  global images
+  images=[pygame.transform.scale(raw_images[i], (game_window.get_width()//(900/209), game_window.get_height()/(500/216))) for i in range(0, 7)]
 def update_buttons() -> None:
   for i in range(65, 91):
+    global buttons
     button=buttons[chr(i)]
     win_width=game_window.get_width()//15*15
     unit=win_width/15
@@ -79,14 +97,22 @@ def update_buttons() -> None:
     button.image = pygame.transform.scale(button.image_raw, (2*button.radius, 2*button.radius))
     button.onclickargs=(button.onclickargs[0])
     buttons[chr(i)]=button
-del win_width, unit, radius, x, y
 if __name__ == "__main__":
   try:
-    word = list(random.choice(words))
-    print(word)
+    word = generate_word(level)
+    print(''.join(word))
     guessed_word=['_']*len(word)
     run = True
     while run:
+      game_window.fill('#FFFFFF')
+      if incorrect>=6:
+        game_window.fill("#FF0000")
+        MultiLineText_Blit(game_window, f"You Lose :( \nThe word was '{''.join(word)}'", ( game_window.get_width()//10*3, game_window.get_height()//7*5), NORMAL_FONT)
+        for button in buttons: buttons[button].should_draw=False
+      update_buttons()
+      update_images()
+      if '_' not in guessed_word:
+        new_level()
       # reset level text
       LEVEL_TEXT=LEVEL_FONT.render(f"Level: {level}", True, '#000000')
       # title
@@ -97,6 +123,8 @@ if __name__ == "__main__":
       game_window.blit(LEVEL_TEXT, (game_window.get_width()-LEVEL_TEXT.get_width(), 2))
       # hanged man
       game_window.blit(images[incorrect], ((game_window.get_width()-images[incorrect].get_width())/2-game_window.get_width()/6//1, TITLE.get_height()+50))
+      # guessed word
+      game_window.blit(NORMAL_FONT.render(''.join(guessed_word), True, '#000000'), ((game_window.get_width())//2-game_window.get_width()//6+images[incorrect].get_width()//3+4, TITLE.get_height()+90))
       for key in buttons: buttons[key].draw(game_window)
       pygame.display.update()
       for event in pygame.event.get():
@@ -109,7 +137,7 @@ if __name__ == "__main__":
     exit()
   finally:
     run=False
-    pygame.quit
+    pygame.quit()
     if dev:
       raise
     exit()
--- games/utils.py
+++ games/utils.py
@@ -4,66 +4,117 @@ Utilities for games
 
 import pygame
 import types
-
+import importlib
+import sys
+from pathlib import Path
+try:
+  from pygame._common import ColorValue, Coordinate
+except ModuleNotFoundError:
+  from typing import Union, Tuple, Sequence
+  RGBOutput=Tuple[int, int, int]
+  RGBAOutput=Tuple[int, int, int, int]
+  Coordinate = Union[Tuple[float, float], Sequence[float], pygame.math.Vector2]
+  ColorValue=Union[pygame.color.Color, int, str, RGBOutput, RGBAOutput, Sequence[int]]
 pygame.init()
-
-class Image(pygame.sprite.Sprite):
-  def __init__(self, x, y, image) -> None:
-    super().__init__()
+class CircleButton:
+  """
+  A PyGame Circular Button.
+  Assumes that you constantly blit in 
+  `while True` game loop
+  """
+  def __init__(self, x: int, y: int, radius: int, thickness: int =1, color: ColorValue= "#000000", \
+        image: pygame.surface.SurfaceType = pygame.font.SysFont(None, 0).render('', True, "#000000"), \
+        onClick: types.FunctionType = (lambda *args, **kwargs: ...), *onClickArgs, **onClickKwargs) -> None:
+    """
+    Initialize The Button
+    """
     self.x = x
     self.y = y
-    self.image = image
-    self.rect = self.image.get_rect()
-class Button:
-    def __init__(self, x: int, y: int, radius: int, thickness: int =1, color = "#000000", \
-                 image: pygame.surface.Surface = pygame.font.SysFont(None, 0).render('', True, "#000000"), \
-                 onClick: types.FunctionType = (lambda *args, **kwargs: ...), *onClickArgs, **onClickKwargs) -> None:
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.thickness = thickness
-        self.color = color
-        self.image_raw = image
-        self.onclick=onClick
-        self.onclickargs=onClickArgs
-        self.onclickkwargs=onClickKwargs
-        self.image_rect = self.image_raw.get_rect()
-        self.image_rect.center = (self.x, self.y)
-        self.image = pygame.transform.scale(self.image_raw, (2*self.radius, 2*self.radius))
+    self.radius = radius
+    self.thickness = thickness
+    self.color = color
+    self.image_raw = image
+    self.onclick=onClick
+    self.onclickargs=onClickArgs
+    self.onclickkwargs=onClickKwargs
+    self.image_rect = self.image_raw.get_rect()
+    self.image_rect.center = (self.x, self.y)
+    self.image = pygame.transform.scale(self.image_raw, (2*self.radius, 2*self.radius))
+    self.clicked=False
+    self.image.blit(self.image, (0,0), (0,0,self.radius*2,self.radius*2))
+    self.should_draw=True
+  def draw(self, screen: pygame.surface.SurfaceType, force_draw=False) -> None:
+    """
+    Draw the button correctly
+    """
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_clicked = pygame.mouse.get_pressed()[0]
+    hover = self.is_hover(mouse_pos)
+    if self.should_draw and not force_draw:
+      pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, self.thickness)
+      screen.blit(self.image, (self.x-self.radius, self.y-self.radius))
+      if hover and mouse_clicked and not self.clicked:
+        self.onclick(*self.onclickargs, **self.onclickkwargs)
+        self.clicked=True
+      elif not mouse_clicked:
         self.clicked=False
-        self.image.blit(self.image, (0,0), (0,0,self.radius*2,self.radius*2))
-        self.should_draw=True
-    def draw(self, screen, force_draw=False) -> None:
-        mouse_pos = pygame.mouse.get_pos()
-        mouse_clicked = pygame.mouse.get_pressed()[0]
-        hover = self.is_hover(mouse_pos)
-        if self.should_draw and not force_draw:
-          pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, self.thickness)
-          screen.blit(self.image, (self.x-self.radius, self.y-self.radius))
-        if hover and mouse_clicked and not self.clicked:
-          self.onclick(*self.onclickargs, **self.onclickkwargs)
-          self.clicked=True
-        elif not mouse_clicked:
-          self.clicked=False
-    
-    def is_hover(self, mouse_pos) -> bool:
-        distance = ((self.x - mouse_pos[0])**2 + (self.y - mouse_pos[1])**2)**0.5
-        return distance <= self.radius
+  
+  def is_hover(self, mouse_pos) -> bool:
+    """
+    Check if mouse is hover over button
+    """
+    distance = ((self.x - mouse_pos[0])**2 + (self.y - mouse_pos[1])**2)**0.5
+    return distance <= self.radius
 class PicklableSurface:
-    def __init__(self, surface) -> None:
-        self.surface = surface
-        self.name = "PicklableSurface"
+  """
+  Representation of pygame.surface.Surface, but it can be pickled
+  """
+  def __init__(self, surface: pygame.surface.SurfaceType) -> None:
+    """
+    Initialize class
+    """
+    self.surface = surface
+    self.name = "PicklableSurface"
 
-    def __getstate__(self) -> dict:
-        state = self.__dict__.copy()
-        surface = state.pop("surface")
-        state["surface_string"] = (pygame.image.tostring(surface, "RGBA"), surface.get_size())
-        return state
+  def __getstate__(self) -> dict:
+    """
+    Send string object through pickle
+    """
+    state = self.__dict__.copy()
+    surface = state.pop("surface")
+    state["surface_string"] = (pygame.image.tostring(surface, "RGBA"), surface.get_size())
+    return state
 
-    def __setstate__(self, state) -> None:
-        surface_string, size = state.pop("surface_string")
-        state["surface"] = pygame.image.fromstring(surface_string, size, "RGBA")
-        self.__dict__.update(state)
-    
-    def __getattr__(self, attr) -> object:
-      return eval(f"self.surface.{attr}")
+  def __setstate__(self, state) -> None:
+    """
+    Get surface object when pickle is setting item
+    """
+    surface_string, size = state.pop("surface_string")
+    state["surface"] = pygame.image.fromstring(surface_string, size, "RGBA")
+    self.__dict__.update(state)
+  
+  def __getattr__(self, attr) -> object:
+    """
+    Help compatibility by forwarding methods to the surface
+    """
+    return eval(f"self.surface.{attr}")
+def MultiLineText_Blit(surface: pygame.surface.SurfaceType, text: str, pos: Coordinate, font: pygame.font.FontType = pygame.font.SysFont(None, 10), color=pygame.Color('black')) -> None:
+  """
+  Blit Muli-Line text to surface.
+  Note: Text can overflow down
+  """
+  words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
+  space = font.size(' ')[0]  # The width of a space.
+  max_width, max_height = surface.get_size()
+  x, y = pos
+  for line in words:
+    for word in line:
+      word_surface = font.render(word, True, color)
+      word_width, word_height = word_surface.get_size()
+      if x + word_width >= max_width:
+        x = pos[0]  # Reset the x.
+        y += word_height  # Start on new row.
+      surface.blit(word_surface, (x, y))
+      x += word_width + space
+    x = pos[0]  # Reset the x.
+    y += word_height  # Start on new row.
